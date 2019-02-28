793a18d6fff6d9e9a2ff879e75f16e325b327d5e
JohanDelValleV <43177845+JohanDelValleV@users.noreply.github.com>
2019-02-28 10:00:03 -0600

    .

--- cliente.py
+++ cliente.py
@@ -7,6 +7,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         print('mensaje:')
         msg=input()
-        s.sendall(msg.encode())
-        data = s.recv(1024)
-        print('Received', repr(data.decode('ascii')))
+        if msg !='':
+            s.sendall(msg.encode())
+            data = s.recv(1024)
+            print('Received', repr(data.decode('ascii')))
