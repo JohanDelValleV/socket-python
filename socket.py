abd7dbacb7250c490c15c62282257e43f75b67fc
JohanDelValleV <43177845+JohanDelValleV@users.noreply.github.com>
2019-02-28 08:48:07 -0600

    .

--- socket.py
+++ socket.py
@@ -1,14 +1,14 @@
 import socket
 
-HOST = ''
+HOST = '192.168.43.107'
 PORT = 3000
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
-while 1:
-    data = conn.recv(1024)
-    if not data: break
-    conn.sendall(data)
-conn.close()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    while 1:
+        data = conn.recv(1024)
+        if not data: break
+        conn.sendall(data)
+    conn.close()
