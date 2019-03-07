5d25c71979545fb7a8f7fb19459e14599aa6df40
JohanDelValleV <43177845+JohanDelValleV@users.noreply.github.com>
2019-03-07 08:26:43 -0600

    FLASK

--- flaskosa.py
+++ flaskosa.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, template_folder='template')
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 @app.route("/")
